5dea134a7b0c682586b59085e38566be90a79b6e
JamieChopra <jwc258@hotmail.co.uk>
2022-04-04 22:51:38 +0100

    Week 4 Exercise 1 and Working on Week 5

--- Week 4/Exercise 1 - Cookies/selectcolour.php
+++ Week 4/Exercise 1 - Cookies/selectcolour.php
@@ -1,9 +1,11 @@
 <html>
   <head><title>Select colour page</title></head>
     <body>
+      <?$cookieName='numberqty';
+        setcookie($cookieName, $_POST[txtqty]); ?>
+      <?echo "Select the colour for the $_POST[txtqty] widgets you are ordering"; ?>
       <form action="confirmation.php"  method="post">
-	Select the colour for the <?php echo $_POST[selqty] ?> widgets you are ordering
-   	<select name="selcolour">
+      <select name="selcolour">
    	  <option>white</option>
 	  <option>red</option>
 	  <option>yellow</option>
@@ -12,6 +14,7 @@
         </select>
         <br/><br/>	
         <input type="submit" value="Buy"/>
+
       </form>
    </body>
 </html>	
---
+++ Week 4/Exercise 1 - Hidden Fields/selectcolour.php
@@ -0,0 +1,20 @@
+<html>
+  <head><title>Select colour page</title></head>
+    <body>
+      <form action="confirmation.php"  method="post">
+	Select the colour for the
+  <input type="hidden" name="hdqty" value="<?php echo $_POST['txtqty'] ?>"/> 
+  widgets you are ordering
+   	<select name="selcolour">
+   	  <option>white</option>
+	  <option>red</option>
+	  <option>yellow</option>
+	  <option>green</option>
+	  <option>blue</option>
+        </select>
+        <br/><br/>	
+        <input type="submit" value="Buy"/>
+
+      </form>
+   </body>
+</html>	
