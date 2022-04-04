031e73e9a89a52d70eef424a810c3fa434fcdd0e
JamieChopra <jwc258@hotmail.co.uk>
2022-04-04 17:20:36 +0100

    Week 2 Exercise 7 and 8

---
+++ wk2ex8.php
@@ -0,0 +1,14 @@
+<?php
+  $topModules[0] = "Internet Systems Development";
+  $topModules[1] = "Programming 1";
+  $topModules[2] = "Programming 2”;
+  $topModules[3] = "OOAD";
+  $topModules[4] = "Software Engineering";
+  $topModules[5] = "Open Source Systems";
+  $topModules[6] = "Mobile Systems Security";
+
+  for($count = 0,$count < 6;$count++);
+  {
+    echo "$count module is $topModules[$count] <br/>";
+  }	
+?>
