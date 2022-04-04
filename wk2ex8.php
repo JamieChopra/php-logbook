bc29d476fa45d11976317926f5f68a9c6628b4e2
JamieChopra <jwc258@hotmail.co.uk>
2022-04-04 17:21:30 +0100

    Update wk2ex8.php

--- wk2ex8.php
+++ wk2ex8.php
@@ -1,11 +1,11 @@
 <?php
-  $topModules[0] = "Internet Systems Development";
-  $topModules[1] = "Programming 1";
-  $topModules[2] = "Programming 2”;
-  $topModules[3] = "OOAD";
-  $topModules[4] = "Software Engineering";
-  $topModules[5] = "Open Source Systems";
-  $topModules[6] = "Mobile Systems Security";
+  $topModules[0] = 'Internet Systems Development';
+  $topModules[1] = 'Programming 1';
+  $topModules[2] = 'Programming 2';
+  $topModules[3] = 'OOAD';
+  $topModules[4] = 'Software Engineering';
+  $topModules[5] = 'Open Source Systems';
+  $topModules[6] = 'Mobile Systems Security';
 
   for($count = 0,$count < 6;$count++);
   {
