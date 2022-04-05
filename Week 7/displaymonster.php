f7e69d8b75dca5ff22c9c91870e7fe9a106cfeac
JamieChopra <jwc258@hotmail.co.uk>
2022-04-05 17:04:06 +0100

    Update displaymonster.php

--- Week 7/displaymonster.php
+++ Week 7/displaymonster.php
@@ -2,13 +2,13 @@
   
   $conn = mysqli_connect('localhost', '22015094', 'mysqluser', 'db1_22015094');
 
-$sql = "select id from monster;";
+$sql = "SELECT id, name from monster;";
 
 $result = mysqli_query($conn, $sql);
 
 $row = mysqli_fetch_array($result);
 
-echo "<img src='getjpg.php?id=" . $row[id]. "'/>";
+echo "<img src='getjpg.php?id=" . $row['id']. "'/>";
 
 mysqli_close($conn);
 ?>
