555726aa7c0c752a59461dd7f2011bc7abe7fed7
JamieChopra <jwc258@hotmail.co.uk>
2022-04-05 16:39:27 +0100

    Week 7 Exercise 3

---
+++ Week 7/displaymonster.php
@@ -0,0 +1,13 @@
+<?php
+  
+  $conn = mysqli_connect('localhost', '22015094', 'mysqluser', 'db1_22015094');
+
+$sql = "select id from monster;";
+
+$result = mysqli_query($sql, $db);
+$row = mysqli_fetch_array($result);
+
+echo "<img src='getjpg.php?id=" . $row[id]. "'/>";
+
+mysqli_close();
+?>
