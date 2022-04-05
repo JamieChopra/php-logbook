a417cecb3c4c9efc3ed8bea997170764d5f2330b
JamieChopra <jwc258@hotmail.co.uk>
2022-04-05 12:56:09 +0100

    Update wk6ex6.php

--- Week 6/wk6ex6.php
+++ Week 6/wk6ex6.php
@@ -1,6 +1,6 @@
 <?php
-	include(“myfunctions.inc”);
-	html_header(“My second function demo”);
-	echo “I pay £ “ . calculatetax(12200,22) . “ tax”;
+	include("myfunctions.inc");
+	html_header("My second function demo");
+	echo "I pay £ " . calculatetax(12200,22) . " tax";
 	html_footer();
 ?>
