11701b3b1ad83c4a1132b3f8804d2d3c2a96ac5d
JamieChopra <jwc258@hotmail.co.uk>
2022-04-05 10:09:26 +0100

    Wk 6 Ex 1

--- Week 6/wk6ex1action.php
+++ Week 6/wk6ex1action.php
@@ -8,10 +8,14 @@
 	$sql = "SELECT * from test";
 	$link= mysqli_connect('localhost', '22015094', 'mysqluser', 'db1_22015094')
 	// Execute sql statement
-	$result = mysqli_query($link, “SELECT * FROM test”);
+	$result = mysqli_query($link, $sql);
 
 	while ($row = mysqli_fetch_assoc($result))
 	{
 		echo "$row[name]  $row[email]  $row[phone_number] <br/>";
 	}
+
+    mysqli_free_result($result);
+    mysqli_close($link);
+
 ?>
