d25598eb806c70a1939a6569dc48a3650185f2c4
JamieChopra <jwc258@hotmail.co.uk>
2022-04-05 12:14:19 +0100

    Update wk6ex2save.php

--- Week 6/wk6ex2save.php
+++ Week 6/wk6ex2save.php
@@ -4,11 +4,11 @@
     $link= mysqli_connect('localhost', '22015094', 'mysqluser', 'db1_22015094');
 
     $sql= "UPDATE test SET name='{$_POST["txtname"]}', 
-        email= '{$_POST["txtemail"]}', 
-        phone_number= '{$_POST["txttelno"]}' 
-        WHERE ID = '{$_GET[id]}';";
+           email= '{$_POST["txtemail"]}', 
+           phone_number= '{$_POST["txttelno"]}' 
+           WHERE ID = '{$_GET["id"]}';";
 
-	$sql = "SELECT * FROM test WHERE ID = '$_GET["id"]' ";
+	//$sql = "SELECT * FROM test WHERE ID = '$_GET["id"]' ";
 	//Execute query
 	$result = mysqli_query($link, "SELECT * FROM test WHERE ID = '$_GET[id]' ");
 
